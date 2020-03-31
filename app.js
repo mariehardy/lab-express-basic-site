82e6fd1fcc479a0bf8a0f1a2c003ff428c25b104
mariehardy <mademoiselle.mph@gmail.com>
2020-03-31 22:09:53 +0200

    BERWDPT Marie - not quite done

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+// We create our own server named app
+// Express server will be handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+// Refers to the folder in which our app.js is located
+console.log(__dirname)
+
+// our first Route
+// GET /home
+app.get('/home', (request, response, next) => {
+    //response.send('awoihdwoiwhd')
+    response.sendFile(__dirname + '/views/home-page.html')
+});
+
+
+app.get('/about', (request, response, next) => {
+    //response.send('awoihdwoiwhd')
+    response.sendFile(__dirname + '/views/about-page.html')
+});
+
+app.get('/works', (request, response, next) => {
+    //response.send('awoihdwoiwhd')
+    response.sendFile(__dirname + '/views/works-page.html')
+});
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
