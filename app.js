0890c17a4a6e5298ed7eee9b62b0701e2cba7be0
mariehardy <mademoiselle.mph@gmail.com>
2020-04-01 12:32:29 +0200

    BERWDPT Marie Lab Completed

--- app.js
+++ app.js
@@ -28,5 +28,10 @@ app.get('/works', (request, response, next) => {
     response.sendFile(__dirname + '/views/works-page.html')
 });
 
+app.get('/gallery', (request, response, next) => {
+    //response.send('awoihdwoiwhd')
+    response.sendFile(__dirname + '/views/gallery-page.html')
+});
+
 // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
